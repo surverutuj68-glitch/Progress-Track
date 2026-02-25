83ab8600a98f0b0c16e158633e26efb0573af149
Rutuj <surverutuj68@gmail.com>
2026-02-26 00:23:35 +0530

    Day 2 done

--- backend-node/server.js
+++ backend-node/server.js
@@ -3,8 +3,24 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Middleware to parse JSON
+app.use(express.json());
+
+// GET route
 app.get('/hello', (req,res)=>{
     res.send('Hello Rutuj 🚀');
+});
+
+// POST route
+app.post('/data', (req,res)=>{
+    //log entire request body
+    console.log("Recieved Data:",req.body);
+
+    //send response back
+    res.json({
+        message: "Data recieved sucessfully!",
+        recievedData: req.body
+    });
 });
 
 app.listen(PORT,()=>{
